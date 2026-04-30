2cd9607a82a5023f16594963005f8121a18f5c23
Sofiia Chorna <chernaya.sofiia@gmail.com>
2026-04-30 16:48:21 +0200

    loseen metatomic ase

--- python/metatomic_torch/setup.py
+++ python/metatomic_torch/setup.py
@@ -330,7 +330,7 @@ if __name__ == "__main__":
         install_requires.append(f"metatomic-ase @ file://{METATOMIC_ASE}")
     else:
         # we are building from a sdist/installing from a wheel
-        install_requires.append("metatomic-ase >=0.1.0,<0.2.0")
+        install_requires.append("metatomic-ase")
 
     setup(
         version=create_version_number(METATOMIC_TORCH_VERSION),
